dc2cc386ce24686a8e3bb7389c7235c48978144d
chandapukiran.kumar <chandapukiran.kumar@sentinelone.com>
2024-02-19 12:51:57 +0530

    renovate config files

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     platform: 'github',
     repositories: [{repository: 'yoyoIgnitor/reno-custom',
                     secrets: {
-                    app_id: process.env.APP_ID,
-                    private_key: process.env.PRIVATE_KEY
+                    app_id: "process.env.APP_ID",
+                    private_key: "process.env.PRIVATE_KEY"
                     }}],
     onboardingConfig: {"$schema": "https://docs.renovatebot.com/renovate-schema.json"},
     extends: ["local>yoyoIgnitor/renovate-custom"],
