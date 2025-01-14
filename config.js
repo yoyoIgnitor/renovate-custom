98bedd258b124e460b5156bb0bd26aa151742dfc
Kiran Chandapu <gkiran.ch@hotmail.com>
2025-01-14 20:30:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,9 @@
 module.exports = {
     platform: 'github',
-    repositories: [{repository: 'yoyoIgnitor/test-custom',
+    repositories: [{repository: 'yoyoIgnitor/go-test',
                     secrets: {
                     app_id: "process.env.APP_ID",
                     private_key: "process.env.PRIVATE_KEY"
                     }}],
     onboardingConfig: {"$schema": "https://docs.renovatebot.com/renovate-schema.json"},
-    extends: ["local>yoyoIgnitor/renovate-custom"],
-    username: 'test', 
-    "hostRules": [
-      {
-        "matchHost": "renotest.jfrog.io",
-        "username": "process.env.RENOVATE_ART_SECRET_USERNAME",
-        "password": "process.env.RENOVATE_ART_SECRET_PASSWORD",
-        "hostType": "docker" 
-      }
-    ]
-  }
+    extends: ["local>yoyoIgnitor/renovate-custom"]
